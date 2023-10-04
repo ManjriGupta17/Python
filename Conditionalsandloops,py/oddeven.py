4f9581024eb826509514a8dbfcb17a0beb045076
ManjriGupta17 <145916283+ManjriGupta17@users.noreply.github.com>
2023-10-04 15:47:02 +0000

    UPDATE

---
+++ Conditionalsandloops,py/oddeven.py
@@ -0,0 +1,5 @@
+number = int(input("Enter a Positive number: ")) 
+if number % 2 ==0:
+    print("It is even")
+else:
+    print("It is odd")
---
+++ bitwiseoperators.py
@@ -0,0 +1,14 @@
+#number1=int(input("enter your number1: "))
+#number2=int(input("enter your number2: "))
+#print("Sum of number1 and number2",number1+number2)
+
+#number5=int(input("enter your number2: "))
+#number6=int(input("enter your number3: "))
+#number4=int(input("enter your number1: "))
+#print("Sum of number1,number2 and number3:- ",number4+number5+number6)
+
+a = 5
+b = 3
+print(" a and b:", a & b)
+print(" a or b:", a | b)
+print(" a xor b:", a ^ b)
