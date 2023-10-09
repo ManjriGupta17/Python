3ac29129191af389dfa01f114c3cbbf3d769318a
ManjriGupta17 <145916283+ManjriGupta17@users.noreply.github.com>
2023-10-09 16:12:28 +0000

    update

--- forloop.py
+++ forloop.py
@@ -2,3 +2,11 @@
 #range(start,end,step)
 for i in range(1,11):
     print(i,"Hello world")
+
+list = [10,20,30,40]
+for i in list:
+    print(i)
+
+fruits = ["Apple","Banana"*5,"Litchi"*10]
+for i in fruits:
+    print(i)
