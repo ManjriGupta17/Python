d3ae8db730ff66ce5abf68aaa8a8c616f0f287e4
ManjriGupta17 <145916283+ManjriGupta17@users.noreply.github.com>
2023-10-09 16:30:14 +0000

    Update

---
+++ whileloop.py
@@ -0,0 +1,22 @@
+i = 2
+while i < 67:
+    print(i)
+    i +=2
+
+i = 100
+while i < 202:
+    print(i)
+    i +=3
+
+
+j = 0
+while j <=10:
+    print(j)
+    j=j+1
+
+i = 10
+while i==20:
+    print("computer buff!")#No output
+
+
+    
