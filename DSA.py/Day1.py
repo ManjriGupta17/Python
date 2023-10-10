2891c17f1f0f104f053b59bdeb0fe8ad02636011
ManjriGupta17 <145916283+ManjriGupta17@users.noreply.github.com>
2023-10-10 14:02:22 +0000

    day 1 notes

---
+++ DSA.py/Day1.py
@@ -0,0 +1,5 @@
+#Raise level of programming,performance
+#IMPORTANCE OF STRUCTURING DATA 
+#EXAMPLE 1)DICTIONARY 2)MAP 3)LEDGER 40 ALMIRAH
+#algorithm-the step by stem linguistic(naturallanguage) representation of logic to solve a given problem
+
