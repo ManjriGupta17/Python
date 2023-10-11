7d67e92cc7bede12e560f5bd6b5ea315e40727d1
ManjriGupta17 <145916283+ManjriGupta17@users.noreply.github.com>
2023-10-11 14:03:02 +0000

    day2

---
+++ DSA.py/day2.py
@@ -0,0 +1,25 @@
+#ARRAY AND LIST
+#it defines an obj type which can efficently represent on array of basic vlues -->floating,integer
+from array import *
+a1=array('i',[23,56,11,90])
+type(a1)
+print(a1)
+a1.pop(0)
+for x in a1:
+    print(x)
+a1.append(21)
+a1
+a1.count(10)
+
+
+
+
+
+
+
+
+
+
+
+
+
